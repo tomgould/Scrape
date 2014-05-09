37dd267e18b0e9931803a42f5e6d7dc95d0f5ecd
Thomas Gould <TomGould01@952860.no-reply.drupal.org>
2014-05-09 11:45:45 +0100

    Moving the directory creation logic so that it only creates a directory if it is required to write a file

--- demo.php
+++ demo.php
@@ -8,15 +8,13 @@ include 'scraper.class.inc';
 
 $scraper = new scraper();
 $scraper->addLocation(
-  'http://dl.best-music.us/Foreign/Arabic/Album/', NULL, array('mp3')
+  'http://dl.best-music.us/Foreign/Arabic/', 'test-x', array('mp3')
 );
 $scraper->setCachePath('/tmp');
 $scraper->setDestinationRoot('/tmp/Scraper-Demo');
 $scraper->excludeInPath('_vti_cnf');
 $scraper->excludeInFilename(array('WTE', 'button', 'hot', 'new', 'arrow', 'smile', 'norr'));
 $scraper->search('nancy');
-$scraper->setMode('search');
-
-var_export($scraper->getSearch());
+$scraper->setMode('test');
 
 $scraper->scrape();
