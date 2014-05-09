1136119b9b5604bc3d51bbbc660380424281d603
Thomas Gould <TomGould01@952860.no-reply.drupal.org>
2014-05-09 10:20:59 +0100

    Adds a search option to the scraper

--- demo.php
+++ demo.php
@@ -8,10 +8,15 @@ include 'scraper.class.inc';
 
 $scraper = new scraper();
 $scraper->addLocation(
-  'http://www.candoo.com/ulsternorrie/images/animated%20gifs/animated%20letters/', 'test-1', array('gif')
+  'http://dl.best-music.us/Foreign/Arabic/Album/', NULL, array('mp3')
 );
 $scraper->setCachePath('/tmp');
 $scraper->setDestinationRoot('/tmp/Scraper-Demo');
 $scraper->excludeInPath('_vti_cnf');
 $scraper->excludeInFilename(array('WTE', 'button', 'hot', 'new', 'arrow', 'smile', 'norr'));
+$scraper->search('nancy');
+$scraper->setMode('search');
+
+var_export($scraper->getSearch());
+
 $scraper->scrape();
