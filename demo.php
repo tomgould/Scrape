46b4b055fcd842a3d02716a7d8f62c2f60d9f70e
tgould <tom.gould@turner.com>
2014-08-29 10:29:52 +0100

    Adds an example of the file name processor to the demo php file

--- demo.php
+++ demo.php
@@ -16,4 +16,22 @@ $scraper->addLocation('http://www.candoo.com/ulsternorrie/images/animated%20gifs
   ->excludeInFilename(array('WTE', 'button', 'hot', 'new', 'arrow', 'smile', 'norr'))
   ->search('CLR')
   ->setMode('search')
+  ->setFileNameProcessor('remove_date_string')
   ->scrape();
+
+/**
+ * Removes a 14 character date string appended to the beginning of file names
+ * if it exists in the name
+ *
+ * eg a file name: "20130130121517-01 - cat.gif"
+ * would become  : "01 - cat.gif"
+ *
+ * @param string $name
+ *
+ * @return string
+ */
+function remove_date_string($name) {
+  if (is_numeric(mb_substr($name, 0, 14))) {
+    return mb_substr($name, ((strlen($name) - 15) * -1));
+  }
+}
